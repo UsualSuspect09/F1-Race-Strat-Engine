39b14cc3b23dc5ceb22c2ecbb6e57236f6673fc6
Vansh Batra <batravansh26@gmail.com>
2026-06-21 23:49:44 +0530

    Simulation engine rework

--- simulator/race_simulator.py
+++ simulator/race_simulator.py
@@ -1,84 +1,3 @@
-# from simulator.stint_predictor import (
-#     StintPredictor
-# )
-
-
-# class RaceSimulator:
-
-#     def __init__(self):
-
-#         self.predictor = (
-#             StintPredictor()
-#         )
-
-#     def simulate_strategy(
-#         self,
-#         strategy,
-#         race_context
-#     ):
-
-#         completed_laps = 0
-
-#         predicted_stints = []
-
-#         race_laps = (
-#             race_context["race_laps"]
-#         )
-
-#         for compound in strategy:
-
-#             race_progress = (
-#                 completed_laps /
-#                 race_laps
-#             )
-
-#             stint_length = (
-#                 self.predictor
-#                 .predict_stint_length(
-#                     compound=compound,
-#                     air_temp=race_context[
-#                         "air_temp"
-#                     ],
-#                     track_temp=race_context[
-#                         "track_temp"
-#                     ],
-#                     season=race_context[
-#                         "season"
-#                     ],
-#                     race_progress=race_progress
-#                 )
-#             )
-
-#             predicted_stints.append(
-#                 stint_length
-#             )
-
-#             completed_laps += (
-#                 round(stint_length)
-#             )
-
-#         total_laps = (
-#             sum(predicted_stints)
-#         )
-
-#         return {
-
-#             "strategy": strategy,
-
-#             "predicted_stints":
-#                 predicted_stints,
-
-#             "total_laps":
-#                 round(total_laps, 1),
-
-#             "pit_stops":
-#                 len(strategy) - 1,
-
-#             "valid":
-#                 total_laps >= race_laps
-
-#         }
-
 from simulator.stint_predictor import (
     StintPredictor
 )
@@ -98,10 +17,6 @@ class RaceSimulator:
         race_context
     ):
 
-        # -------------------------
-        # Initialize
-        # -------------------------
-
         completed_laps = 0
 
         predicted_stints = []
@@ -112,11 +27,11 @@ class RaceSimulator:
             race_context["race_laps"]
         )
 
-        # -------------------------
-        # Simulate Each Stint
-        # -------------------------
-
         for compound in strategy:
+
+            # -------------------------
+            # NORMALIZED RaceProgress
+            # -------------------------
 
             race_progress = (
                 completed_laps /
@@ -128,6 +43,17 @@ class RaceSimulator:
                     race_progress,
                     3
                 )
+            )
+
+            print("-" * 50)
+            print(
+                f"Compound: {compound}"
+            )
+            print(
+                f"Completed Laps: {completed_laps}"
+            )
+            print(
+                f"Race Progress: {race_progress:.3f}"
             )
 
             stint_length = (
@@ -147,6 +73,10 @@ class RaceSimulator:
                 )
             )
 
+            print(
+                f"Predicted Stint: {stint_length}"
+            )
+
             predicted_stints.append(
                 round(
                     stint_length,
@@ -159,10 +89,6 @@ class RaceSimulator:
                     stint_length
                 )
             )
-
-        # -------------------------
-        # Final Metrics
-        # -------------------------
 
         total_laps = round(
             sum(
